62f69736cc1bbb3a9bf075eb11a8175ad27fa90a
abhishek-shukla-github <abhishekshuklaJS@gmail.com>
2021-08-23 20:48:42 +0530

    Add server side props fetching

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,12 +1,26 @@
 import { useRouter } from "next/router";
-const article = () => {
+import Link from "next/link";
+
+
+const article = ({ article }) => {
     const router = useRouter();
     const { id } = router.query;
     return (
         <div>
-            <h1>This is the article Page for id {id}!</h1>
+            <h1>{article.title}</h1>
+            <p>{article.body}</p>
+            <Link href="/">Go Back</Link>
         </div>
     )
+}
+
+export const getServerSideProps = async (context) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
+    const article = await res.json();
+
+    return {
+        props: { article }
+    }
 }
 
 export default article;
